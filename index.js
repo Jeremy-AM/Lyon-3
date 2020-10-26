9a3734b378bd58ffd7b9daf57d5b8ca87c135738
Jérémy Brunet <63155312+Jeremy-AM@users.noreply.github.com>
2020-10-26 07:58:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/learning-area/javascript/apis/client-side-storage/cache-sw/video-store-offline/sw.js')
+             .register('sw.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 };
