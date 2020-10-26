ce01233496d062f5cafb42179fd8e05172bb29b4
Jérémy Brunet <63155312+Jeremy-AM@users.noreply.github.com>
2020-10-26 07:58:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('sw.js')
+             .register('/sw.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 };
